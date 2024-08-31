fdd9488e9971aaf9d411e4572fdff372d81162d1
Nithyasree8 <113105796+Nithyasree8@users.noreply.github.com>
2024-08-31 09:34:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+//your JS code here. If required
+var modal = document.getElementById("myModal");
+var openModalBtn = document.getElementById("openModal");
+var closeModalBtn = document.getElementsByClassName("close-modal")[0];
+
+// When the user clicks the button, open the modal
+openModalBtn.onclick = function() {
+    modal.style.display = "block";
+}
+
+
+closeModalBtn.onclick = function() {
+    modal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
